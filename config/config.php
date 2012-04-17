8e50ed96e6e14e2ddbf0096c8d04af3c2dbfec8f
Jukka Svahn <web@rahinaa.biz>
2012-04-18 00:44:37 +0300

    Setting rah_flat_cfg to FALSE actually disables rah_flat.

--- config/config.php
+++ config/config.php
@@ -4,19 +4,21 @@
  * @const rah_flat_cfg Path to configuration file.
  *
  * Sets where the configuration file, rah_flat.config.xml, is located.
- * If set to FALSE or undefined, rah_flat.config.xml is looked from
- * textpattern installation directory, e.g. /textpattern/, the directory host
- * config.php and publish.php etc.
+ * If undefined, rah_flat.config.xml is looked from Textpattern's installation 
+ * directory, e.g. /textpattern/, the directory that hosts config.php and publish.php
+ * files.
  *
- * If rah_flat.config.xml is located somewhere else, which it should, place the line
+ * If rah_flat.config.xml is located somewhere else (which it should) place the line
  * below to your Textpattern's configuration file (/textpattern/config.php). Used value
  * should be an absolute filesystem path.
  *
  * <code>
  *		define('rah_flat_cfg', '/home/user/path/to/rah_flat.config.xml');
  * </code>
+ *
+ * If the constant is set to FALSE, rah_flat's functionality is disabled.
  */
 
-	define('rah_flat_cfg', false);
+	define('rah_flat_cfg', '/absolute/path/to/rah_flat.config.xml');
 
 ?>
