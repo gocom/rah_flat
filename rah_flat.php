0e81643503f900add9dfb99d28ec55347e052cbc
Jukka Svahn <web@rahinaa.biz>
2013-07-06 15:31:16 +0300

    Fix typo in the header.

--- rah_flat.php
+++ rah_flat.php
@@ -8,7 +8,7 @@
  * @link    https://github.com/gocom/rah_flat
  *
  * Copyright (C) 2013 Jukka Svahn http://rahforum.biz
- * Licensed under GNU Genral Public License version 2
+ * Licensed under GNU General Public License version 2
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
 
