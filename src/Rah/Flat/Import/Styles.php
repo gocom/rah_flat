76527c58187d811d2188f4819ce0fb43dbe61a5d
Jukka Svahn <web@rahinaa.biz>
2015-02-19 19:16:28 +0200

    Update header dates.

--- src/Rah/Flat/Import/Styles.php
+++ src/Rah/Flat/Import/Styles.php
@@ -4,7 +4,7 @@
  * rah_flat - Flat templates for Textpattern CMS
  * https://github.com/gocom/rah_flat
  *
- * Copyright (C) 2014 Jukka Svahn
+ * Copyright (C) 2015 Jukka Svahn
  *
  * This file is part of rah_flat.
  *
